622e6f5b80af720d56a4b49539c6c639546917d9
Mindaugas Liubinas <mli@synoint.com>
2019-12-16 10:29:23 +0200

    Make a place for app controllers

--- src/Syno/Storm/Controller/IndexController.php
+++ src/Syno/Storm/Controller/IndexController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Syno\Storm\Api\Controller;
+namespace Syno\Storm\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
