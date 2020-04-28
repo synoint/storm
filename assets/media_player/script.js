8db8e7cdf956bffa9a892a4a98e7936e591b6833
gediminas <gka@synoint.com>
2020-04-28 14:21:05 +0300

    Add 'Plyr' media player

---
+++ assets/media_player/script.js
@@ -0,0 +1,11 @@
+import Plyr from 'plyr';
+
+class MediaPlayer {
+    constructor() {
+        const player = new Plyr('.media-player');
+    }
+}
+
+(function () {
+    let mediaPlayer = new MediaPlayer();
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ Encore
     // .addEntry('materialize.common', './assets/materialize/common/index.js')
     .addEntry('b4.common', './assets/b4/common/index.js')
     .addEntry('b4.page', './assets/b4/page/index.js')
+    .addEntry('media.player', './assets/media_player/script.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
