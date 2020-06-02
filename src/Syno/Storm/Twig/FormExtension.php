7a5f834a3d165108ecfb9e0c5ff2a3eaf91fb951
zygimantas <zba@synoint.com>
2020-06-02 16:17:07 +0300

    Changes regarding code review.

--- src/Syno/Storm/Twig/FormExtension.php
+++ src/Syno/Storm/Twig/FormExtension.php
@@ -25,30 +25,27 @@ class FormExtension extends AbstractExtension
      */
     public function shuffleQuestionAnswers(FormView $form, Question $question)
     {
-        if ($question->getRandomizeAnswers()) {
-            shuffle($form->vars['form']->children);
-            $exclusives      = [];
-            $freeTextAnswers = [];
-            foreach ($form->vars['form']->children as $index => $child) {
-                if ($question->getAnswer($child->vars['value'])->getIsExclusive()) {
-                    $exclusives[] = $child;
-                    unset($form->vars['form']->children[$index]);
-                }
-                if ($question->getAnswer($child->vars['value'])->getIsFreeText()) {
-                    $freeTextAnswers[] = $child;
-                    unset($form->vars['form']->children[$index]);
-                }
+        shuffle($form->vars['form']->children);
+        $exclusives      = [];
+        $freeTextAnswers = [];
+        foreach ($form->vars['form']->children as $index => $child) {
+            if ($question->getAnswer($child->vars['value'])->getIsExclusive()) {
+                $exclusives[] = $child;
+                unset($form->vars['form']->children[$index]);
             }
-            foreach($freeTextAnswers as $freeTextAnswer) {
-                $form->vars['form']->children[] = $freeTextAnswer;
+            if ($question->getAnswer($child->vars['value'])->getIsFreeText()) {
+                $freeTextAnswers[] = $child;
+                unset($form->vars['form']->children[$index]);
             }
-            foreach($exclusives as $exclusive) {
-                $form->vars['form']->children[] = $exclusive;
-            }
-
-            return $form;
-        } else {
-            return $form;
         }
+        // Exclusive should be last because exclusive is answer option 'none' which is always the last one.
+        foreach($freeTextAnswers as $freeTextAnswer) {
+            $form->vars['form']->children[] = $freeTextAnswer;
+        }
+        foreach($exclusives as $exclusive) {
+            $form->vars['form']->children[] = $exclusive;
+        }
+
+        return $form;
     }
 }
