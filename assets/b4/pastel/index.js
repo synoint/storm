36da5ba134015451722a3b1c9e9f7ea05b395f30
Sung <sca@synoint.com>
2020-06-30 10:24:20 +0300

    First template

--- assets/b4/pastel/index.js
+++ assets/b4/pastel/index.js
@@ -1,13 +1,18 @@
 // common styles
-require('./style.css');
+require("../common/b4.css");
+require("../common/style.css");
 // application overrides
-require('./app.css');
+require("./app.css");
 
-const $ = require('jquery');
+const $ = require("jquery");
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
-require('bootstrap');
+require("bootstrap");
 
 // common images
-const imagesContext = require.context('./images', true, /\.(png|jpg|jpeg|gif|ico|svg|webp)$/);
+const imagesContext = require.context(
+	"./images",
+	true,
+	/\.(png|jpg|jpeg|gif|ico|svg|webp)$/
+);
 imagesContext.keys().forEach(imagesContext);
