498e65750b72413339e44f724a7df75762c0e548
gediminas <gka@synoint.com>
2020-09-25 10:09:04 +0300

    Fix required to watch and listen media before proceeding

--- assets/media_player/script.js
+++ assets/media_player/script.js
@@ -2,41 +2,45 @@ import "../../node_modules/plyr/src/sass/plyr.scss";
 import Plyr from "plyr";
 
 class MediaPlayer {
-	constructor(el) {
-		this.player = new Plyr("#media-player");
-		this.container = el;
-	}
+	constructor(el, required) {
 
-	hideControls() {
-		this.container.addEventListener("ready", (event) => {
-			const player = event.detail.plyr;
-			player.config.hideControls = false;
-		});
-	}
-	toggleControls(val) {
-		this.player.toggleControls(val);
+		let options = {};
+
+		if(required) {
+			options = {
+				listeners: {
+					seek: function (e) {
+						e.preventDefault()
+						return false;
+					}
+				}
+			};
+		}
+
+		this.player = new Plyr("#media-player", options);
+		this.container = el;
 	}
 }
 
 (function () {
 	const container = document.querySelector("#media-player");
-	const mediaplayer = new MediaPlayer(container);
-	const fullscreen = mediaplayer.container.getAttribute("fullscreen");
-	const required = mediaplayer.container.getAttribute("required");
+	const required = container.getAttribute("data-required");
+	const autoplay = container.getAttribute("data-autoplay");
+	const mediaPlayer = new MediaPlayer(container, required);
 
-	if (fullscreen === "false") {
-		const button = document.querySelector('button[data-plyr="fullscreen"]');
-		button.style = "display:none";
+	if(autoplay === "true"){
+		mediaPlayer.player.on('ready', () => {
+			mediaPlayer.player.muted = true;
+			mediaPlayer.player.play();
+		})
 	}
 
 	if (required === "true") {
 		const button = document.querySelector("#page_next");
 		button.setAttribute("disabled", "true");
-		mediaplayer.toggleControls(false);
-		mediaplayer.hideControls();
-		mediaplayer.player.on("ended", (ev) => {
+
+		mediaPlayer.player.on("ended", (ev) => {
 			button.removeAttribute("disabled");
-			mediaplayer.toggleControls(true);
 		});
 	}
 })();
