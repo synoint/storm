9203bc9dbd83237b83895a9cd790a9eb6789a5c0
Gediminas Kazlauskas <gka@synoint.com>
2022-04-26 15:01:29 +0300

    Add suppoert for multiple media players in one page

--- assets/media_player/script.js
+++ assets/media_player/script.js
@@ -17,30 +17,59 @@ class MediaPlayer {
 			};
 		}
 
-		this.player = new Plyr("#media-player", options);
+		this.player = new Plyr(el, options);
 		this.container = el;
 	}
 }
 
 (function () {
+
+	const containers = document.getElementsByClassName('media-player');
+
+	containers.forEach(function(el, index, array){
+		const required = el.getAttribute("data-required");
+		const autoplay = el.getAttribute("data-autoplay");
+		const mediaPlayer = new MediaPlayer(el, required);
+
+		if(autoplay === "true"){
+			mediaPlayer.player.on('ready', () => {
+				mediaPlayer.player.muted = true;
+				mediaPlayer.player.play();
+			})
+		}
+
+		if (required === "true") {
+			const button = document.querySelector("#p_next");
+			button.setAttribute("disabled", "true");
+
+			mediaPlayer.player.on("ended", (ev) => {
+				button.removeAttribute("disabled");
+			});
+		}
+	});
+	
 	const container = document.querySelector("#media-player");
-	const required = container.getAttribute("data-required");
-	const autoplay = container.getAttribute("data-autoplay");
-	const mediaPlayer = new MediaPlayer(container, required);
 
-	if(autoplay === "true"){
-		mediaPlayer.player.on('ready', () => {
-			mediaPlayer.player.muted = true;
-			mediaPlayer.player.play();
-		})
-	}
+	if(container !== null){
 
-	if (required === "true") {
-		const button = document.querySelector("#page_next");
-		button.setAttribute("disabled", "true");
+		const required = container.getAttribute("data-required");
+		const autoplay = container.getAttribute("data-autoplay");
+		const mediaPlayer = new MediaPlayer(container, required);
 
-		mediaPlayer.player.on("ended", (ev) => {
-			button.removeAttribute("disabled");
-		});
+		if(autoplay === "true"){
+			mediaPlayer.player.on('ready', () => {
+				mediaPlayer.player.muted = true;
+				mediaPlayer.player.play();
+			})
+		}
+
+		if (required === "true") {
+			const button = document.querySelector("#p_next");
+			button.setAttribute("disabled", "true");
+
+			mediaPlayer.player.on("ended", (ev) => {
+				button.removeAttribute("disabled");
+			});
+		}
 	}
 })();
