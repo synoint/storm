e6ccafd5051715687fc29421b83f3631eb0ed8ef
Mindaugas Liubinas <mli@synoint.com>
2023-12-14 16:21:42 +0200

    Add caching for dataLayer answer map

--- src/Syno/Storm/Services/ResponseDataLayer.php
+++ src/Syno/Storm/Services/ResponseDataLayer.php
@@ -4,17 +4,14 @@ declare(strict_types=1);
 namespace Syno\Storm\Services;
 
 use Syno\Storm\Document;
-use Syno\Storm\Traits\HtmlAware;
 
 class ResponseDataLayer
 {
-    use HtmlAware;
+    private SurveyAnswerMap $surveyAnswerMap;
 
-    private Page $pageService;
-
-    public function __construct(Page $pageService)
+    public function __construct(SurveyAnswerMap $surveyAnswerMap)
     {
-        $this->pageService = $pageService;
+        $this->surveyAnswerMap = $surveyAnswerMap;
     }
 
     public function getData(Document\Survey $survey, Document\Response $response): array
@@ -41,87 +38,28 @@ class ResponseDataLayer
     {
         $result = [];
 
-        $surveyAnswerMap = $this->getSurveyAnswerMap(
-            $survey->getSurveyId(),
-            $survey->getVersion(),
-            $response->getLocale()
-        );
+        if (!$response->getAnswers()->isEmpty()) {
+            $surveyAnswerMap = $this->surveyAnswerMap->get(
+                $survey->getSurveyId(),
+                $survey->getVersion(),
+                $response->getLocale()
+            );
 
-        /** @var Document\ResponseAnswer $responseAnswer */
-        foreach ($response->getAnswers() as $responseAnswer) {
-            /** @var Document\ResponseAnswerValue $responseAnswerValue */
-            foreach ($responseAnswer->getAnswers() as $responseAnswerValue) {
-                $answer = $surveyAnswerMap[$responseAnswerValue->getAnswerId()] ?? [];
-                if (null !== $responseAnswerValue->getValue()) {
-                    $answer['value'] = $responseAnswerValue->getValue();
-                }
-                $result[] = $answer;
-            }
-        }
-
-        return $result;
-    }
-
-    private function getSurveyAnswerMap(int $surveyId, int $version, string $locale): array
-    {
-        $result = [];
-        $surveyAnswers = $this->pageService->getAnswersForDataLayer($surveyId, $version);
-        if ($surveyAnswers) {
-             $result = $this->convertToMapByAnswerId($surveyAnswers, $locale);
-        }
-
-        return $result;
-    }
-
-    private function convertToMapByAnswerId($answerData, string $locale): array
-    {
-        $map = [];
-        foreach ($answerData as $page) {
-            if (!isset($page['questions'])) {
-                continue;
-            }
-            foreach ($page['questions'] as $question) {
-                if (!isset($question['answers'])) {
-                    continue;
-                }
-                foreach ($question['answers'] as $answer) {
-
-                    $mapItem = [
-                        'pageCode'     => $page['code'],
-                        'questionCode' => $question['code'],
-                        'questionText' => !empty($question['text']) ? $this->cleanMarkup($question['text']) : '',
-                        'questionType' => $question['questionTypeId']
-                    ];
-
-                    $translations = $answer['translations'] ?? [];
-
-                    if (isset($answer['rowCode']) || isset($answer['columnCode'])) {
-                        $mapItem['rowCode']    = $answer['rowCode'] ?? null;
-                        $mapItem['rowLabel']   = $this->translate($locale, $translations, 'rowLabel');
-                        $mapItem['columnCode'] = $answer['columnCode'] ?? null;
-                        $mapItem['columnLabel'] = $this->translate($locale, $translations, 'columnLabel');
-                    } else {
-                        $mapItem['code']  = $answer['code'] ?? null;
-                        $mapItem['label'] = $this->translate($locale, $translations, 'label');
+            /** @var Document\ResponseAnswer $responseAnswer */
+            foreach ($response->getAnswers() as $responseAnswer) {
+                /** @var Document\ResponseAnswerValue $responseAnswerValue */
+                foreach ($responseAnswer->getAnswers() as $responseAnswerValue) {
+                    $answer = $surveyAnswerMap[$responseAnswerValue->getAnswerId()] ?? [];
+                    if (null !== $responseAnswerValue->getValue()) {
+                        $answer['value'] = $responseAnswerValue->getValue();
                     }
-
-                    $answerId = (int) $answer['answerId'];
-                    $map[$answerId] = $mapItem;
+                    $result[] = $answer;
                 }
             }
         }
 
-        return $map;
+        return $result;
     }
 
-    private function translate(string $locale, array $translations, string $key):? string
-    {
-        foreach ($translations as $translation) {
-            if ($translation['locale'] === $locale && isset($translation[$key])) {
-                return $translation[$key];
-            }
-        }
 
-        return null;
-    }
 }
---
+++ src/Syno/Storm/Services/SurveyAnswerMap.php
@@ -0,0 +1,93 @@
+<?php
+declare(strict_types=1);
+
+namespace Syno\Storm\Services;
+
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
+use Syno\Storm\Traits\HtmlAware;
+
+class SurveyAnswerMap
+{
+    use HtmlAware;
+
+    private Page                   $pageService;
+    private TagAwareCacheInterface $cache;
+
+    public function __construct(Page $pageService, TagAwareCacheInterface $surveyCache)
+    {
+        $this->pageService = $pageService;
+        $this->cache       = $surveyCache;
+    }
+
+    public function get(int $surveyId, int $version, string $locale): array
+    {
+        $key = 'answer:map:' . $surveyId . ':' . $version . ':' . $locale;
+
+        return $this->cache->get($key, function (ItemInterface $item) use ($surveyId, $version, $locale) {
+            $item->expiresAfter(900);
+
+            $result = [];
+            $surveyAnswers = $this->pageService->getAnswersForDataLayer($surveyId, $version);
+            if ($surveyAnswers) {
+                $result = $this->convertToMapByAnswerId($surveyAnswers, $locale);
+            }
+
+            return $result;
+        });
+    }
+
+    private function convertToMapByAnswerId($answerData, string $locale): array
+    {
+        $map = [];
+        foreach ($answerData as $page) {
+            if (!isset($page['questions'])) {
+                continue;
+            }
+            foreach ($page['questions'] as $question) {
+                if (!isset($question['answers'])) {
+                    continue;
+                }
+                foreach ($question['answers'] as $answer) {
+
+                    $mapItem = [
+                        'pageCode'     => $page['code'],
+                        'questionCode' => $question['code'],
+                        'questionText' => !empty($question['text']) ? $this->cleanMarkup($question['text']) : '',
+                        'questionType' => $question['questionTypeId']
+                    ];
+
+                    $translations = $answer['translations'] ?? [];
+
+                    if (isset($answer['rowCode']) || isset($answer['columnCode'])) {
+                        $mapItem['rowCode']    = $answer['rowCode'] ?? null;
+                        $mapItem['rowLabel']   = $this->translate($locale, $translations, 'rowLabel');
+                        $mapItem['columnCode'] = $answer['columnCode'] ?? null;
+                        $mapItem['columnLabel'] = $this->translate($locale, $translations, 'columnLabel');
+                    } else {
+                        $mapItem['code']  = $answer['code'] ?? null;
+                        $mapItem['label'] = $this->translate($locale, $translations, 'label');
+                    }
+
+                    $answerId = (int) $answer['answerId'];
+                    $map[$answerId] = $mapItem;
+                }
+            }
+        }
+
+        return $map;
+    }
+
+    private function translate(string $locale, array $translations, string $key):? string
+    {
+        foreach ($translations as $translation) {
+            if ($translation['locale'] === $locale && isset($translation[$key])) {
+                return $translation[$key];
+            }
+        }
+
+        return null;
+    }
+
+
+}
