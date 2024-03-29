0c9fc96192fea1c6f263760e2557ba9b0677dd49
Aivaras Riskus <ari@synoint.com>
2024-03-29 13:27:01 +0200

    Moved translations to validators

--- src/Syno/Storm/Validator/Constraints/OtherFilled.php
+++ src/Syno/Storm/Validator/Constraints/OtherFilled.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Syno\Storm\Validator\Constraints;
 
 use Symfony\Component\Validator\Constraint;
@@ -8,14 +9,15 @@ use Symfony\Component\Validator\Exception\MissingOptionsException;
 
 class OtherFilled extends Constraint
 {
-    public string $message = 'error.selected.option.text.required';
+    public string $message           = 'error.selected.option.text.required';
     public Answer $answer;
     public ?array $respondentAnswers = [];
 
     public function __construct($options)
     {
         if (null === $options['answer']) {
-            throw new MissingOptionsException(sprintf('Option "answer" must be given for constraint %s', __CLASS__), ['answer']);
+            throw new MissingOptionsException(sprintf('Option "answer" must be given for constraint %s', __CLASS__),
+                ['answer']);
         }
 
         $this->answer            = $options['answer'];
@@ -26,7 +28,7 @@ class OtherFilled extends Constraint
 
     public function validatedBy(): string
     {
-        return \get_class($this).'Validator';
+        return \get_class($this) . 'Validator';
     }
 
     public function getTargets(): string
