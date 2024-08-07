0bbe6e23e19398192f97b036357b0951d77f7b88
Gediminas Kazlauskas <gka@synoint.com>
2024-08-07 16:13:14 +0300

    Fix single choice matrix with 0 as value code

--- src/Syno/Storm/Form/PageType.php
+++ src/Syno/Storm/Form/PageType.php
@@ -300,7 +300,7 @@ class PageType extends AbstractType
                 'choices'     => $choices,
                 'multiple'    => $multiple,
                 'expanded'    => true,
-                'data'        => $data,
+                'data'        => $data !== false ? $data : null,
                 'placeholder' => null,
                 'label_html'  => true,
                 'required'    => $question->isRequired(),
